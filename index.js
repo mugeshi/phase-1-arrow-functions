9845d9ce6175acab36bedf4741c45b59d684b22c
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-15 18:02:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => {
+  return num * num;
+};
+
+// Arrow function called add
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+// Test cases
+describe("index.js", () => {
+  it("has a function expression called divide", () => {
+    expect(divide).to.exist;
+  });
+
+  it("divide divides 2000 by 100", () => {
+    expect(divide()).to.equal(20);
+  });
+
+  it("has an arrow function called square", () => {
+    expect(square).to.exist;
+  });
+
+  it("square arrow function takes one parameter and multiplies it times itself", () => {
+    let x = 2;
+    expect(square(x)).to.equal(4);
+  });
+
+  it("has an arrow function called add", () => {
+    expect(add).to.exist;
+  });
+
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3;
+    let b = 4;
+    expect(add(a, b)).to.equal(a + b);
+  });
+});
